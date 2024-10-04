6aa4f418208ff1ba017730dd6908f8d18839e89c
Sanjana <160689416+Sanjana141@users.noreply.github.com>
2024-10-04 19:05:24 +0530

    problem3.js

--- problem3.js
+++ problem3.js
@@ -1,3 +1,9 @@
+1. We have two integers m and n, calculate the sum of the cubes of integers from m to n.
+           If m>n return 0 or display the proper message 
+            test case 1:
+                                   m = 2,n=4
+                                   2^3+3^3+4^3=99 
+
 let m=prompt("Enter the value of m:")
 let n=prompt("Enter the value of n:")
 let sum=0
